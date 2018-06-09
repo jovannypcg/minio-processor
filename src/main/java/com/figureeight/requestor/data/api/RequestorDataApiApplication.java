aa965cf43981b47f7c958dabe0ab5dc6c391d4e2
Jovanny Cruz <jovannypcg@yahoo.com>
2018-06-09 15:07:37 -0500

    First commit

---
+++ src/main/java/com/figureeight/requestor/data/api/RequestorDataApiApplication.java
@@ -0,0 +1,11 @@
+package com.figureeight.requestor.data.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestorDataApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RequestorDataApiApplication.class, args);
+    }
+}
---
+++ src/test/java/com/figureeight/requestor/data/api/RequestorDataApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.figureeight.requestor.data.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RequestorDataApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
