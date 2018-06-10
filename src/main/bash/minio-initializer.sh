421f6839b85688c17d524c06a222d916b75da30d
Jovanny Cruz <jovannypcg@yahoo.com>
2018-06-10 13:23:24 -0500

    Fix ARN for events in RabbitMQ

--- src/main/bash/minio-initializer.sh
+++ src/main/bash/minio-initializer.sh
@@ -3,5 +3,4 @@
 mc config host add minio http://minio-server:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 mc mb minio/f8-requestor-data
 
-# TODO: Fix ARN
-mc events add minio/f8-requestor-data arn:minio:sqs:us-east-1:1:uploadEvents --suffix .rb
+mc events add minio/f8-requestor-data arn:minio:sqs:us-east-1:1:amqp --suffix .rb
