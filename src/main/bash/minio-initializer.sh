cd4e2c9a819f2d4e15843b0bf6ef9a0351c92e62
Jovanny Cruz <jovannypcg@yahoo.com>
2018-06-10 13:36:37 -0500

    Change event for minio bucket to listen to .csv files

--- src/main/bash/minio-initializer.sh
+++ src/main/bash/minio-initializer.sh
@@ -3,4 +3,4 @@
 mc config host add minio http://minio-server:9000 ${MINIO_ACCESS_KEY} ${MINIO_SECRET_KEY}
 mc mb minio/f8-requestor-data
 
-mc events add minio/f8-requestor-data arn:minio:sqs:us-east-1:1:amqp --suffix .rb
+mc events add minio/f8-requestor-data arn:minio:sqs:us-east-1:1:amqp --suffix .csv
